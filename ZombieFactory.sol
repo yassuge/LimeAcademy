3371414b50db21f6bbff463267e70a1fb7aaf37b
Yassine <yassine@indexifi.io>
2022-10-04 20:34:53 +0100

    test

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.7;
 
 contract ZombieFactory {
-    // declare our event here
+
+    event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
@@ -14,9 +15,15 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
-        // and fire it here
+        uint id = zombies.length - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
@@ -25,8 +32,10 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
 
 }
+
