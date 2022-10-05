816cde7fb99bbc44161008c6a64183aff9f9c4f1
Yassine <yassine@indexifi.io>
2022-10-05 14:03:11 +0100

    lesson 3

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -39,6 +39,7 @@ contract ZombieFactory is Ownable {
     function createRandomZombie(string memory _name) public {
         require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
 
