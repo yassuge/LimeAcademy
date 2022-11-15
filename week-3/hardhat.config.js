7536c6a91a8a20ea080cb965c912f2a870b8cacb
Yassine <yassine@indexifi.io>
2022-11-15 13:39:15 +0000

    few changes

---
+++ week-3/hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  // solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
+
+task("deploy-testnets", "Deploys contract on a provided network")
+    .setAction(async (taskArguments, hre, runSuper) => {
+        const deployElectionContract = require("./scripts/deploy");
+        await deployElectionContract(taskArguments);
+    });
+
+subtask("print", "Prints a message")
+    .addParam("message", "The message to print")
+    .setAction(async (taskArgs) => {
+      console.log(taskArgs.message);
+    });
