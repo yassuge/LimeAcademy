b636f8f9ced0b96496c68547234a3d940c5f8048
Yassine <yassine@indexifi.io>
2022-11-15 16:37:11 +0000

    f

--- week-2/hardhat.config.js
+++ week-2/hardhat.config.js
@@ -2,13 +2,13 @@ require("@nomicfoundation/hardhat-toolbox");
 
 // Go to https://www.alchemyapi.io, sign up, create
 // a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = "KEY";
+const INFURA_API_KEY = "6e68db481a01433080e0e811ef4b83a9";
 
 // Replace this private key with your Goerli account private key
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const GOERLI_PRIVATE_KEY = "YOUR GOERLI PRIVATE KEY";
+const GOERLI_PRIVATE_KEY = "e804168f0f331ae5d48419ca4ad4193e75188ad4c42bb9f009b1f36ade0710ad";
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -16,7 +16,7 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [GOERLI_PRIVATE_KEY]
     }
   }
