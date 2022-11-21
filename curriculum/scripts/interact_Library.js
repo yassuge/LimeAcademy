29357f1437f0a697b2422de2250a90e2650fe03b
Yassine <yassine@indexifi.io>
2022-11-21 10:39:30 +0000

    refactoring

--- curriculum/scripts/interact_Library.js
+++ curriculum/scripts/interact_Library.js
@@ -18,19 +18,22 @@ const run = async function() {
     console.log(hre.ethers.utils.formatEther(balance, 18))
     console.log(balance.toString())
 
+    // retreive contract
     const contractAddress = "0x9A676e781A523b5d0C0e43731313A708CB607508"
     const libraryContract = new hre.ethers.Contract(contractAddress, BookLibrary.abi, wallet)
-    // console.log(libraryContract)
-
+    
+    // display owner of the contract
     console.log(await libraryContract.owner())
 
-    // const transactionBookCreated = await libraryContract.addBook("Book_0", 10)
-    // console.log(transactionBookCreated)
+    // add book to inventory
+    const transactionBookCreated = await libraryContract.addBook("Book_0", 10)
 
+    // get inventory
     const availableBooks = await libraryContract.getInventory("Book_0")
     console.log("available books of Book_0 are : ", availableBooks)
 
-    const transactionRentBook = await libraryContract.borrowBook("User_6","Book_0")
+    // User_0 borrows Book_0
+    const transactionRentBook = await libraryContract.borrowBook("User_0","Book_0")
     const transactionReceipt = await transactionRentBook.wait();
     if (transactionReceipt.status != 1) { // 1 means success
         console.log("Transaction was not successful")
@@ -52,28 +55,6 @@ const run = async function() {
     console.log(transactionRetReceipt)
     const availablePostRetBooks = await libraryContract.getInventory("Book_0")
     console.log("available books of Book_0 are post borrow: ", availablePostRetBooks)
-
-
-    // const hasEnded = await electionContract.electionEnded()
-    // console.log("The election has ended:", hasEnded)
-
-    // const haveResultsForOhio = await electionContract.resultsSubmitted("Ohio")
-    // console.log("Have results for Ohio:", haveResultsForOhio)
-
-    // // sending transactions
-    // const transactionOhio = await electionContract.submitStateResult(["titi", 250, 150, 24])
-    // const transactionReceipt = await transactionOhio.wait();
-    // if (transactionReceipt.status != 1) { // 1 means success
-    //     console.log("Transaction was not successful")
-    //     return 
-    // }
-
-    // const resultsSubmittedOhioNew = await electionContract.resultsSubmitted("titi")
-    // console.log("Results submitted for titi", resultsSubmittedOhioNew)
-
-    // const currentLeader = await electionContract.currentLeader()
-    // console.log("Current leader", currentLeader)
-
 }
 
 run()
