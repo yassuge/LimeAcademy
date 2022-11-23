f0b64ac96ae70dc98765eeb3fa1073ead27d017d
Yassine <yassine@indexifi.io>
2022-11-23 16:18:43 +0000

    interact_ETHWRapper draft to be fixed later

--- curriculum/scripts/interact_ETHWrapper.js
+++ curriculum/scripts/interact_ETHWrapper.js
@@ -1,23 +1,29 @@
 const keys = require("../../keys.json")
-const PRIVATE_KEY = keys.PRIVATE_KEY
+const PRIVATE_KEY = keys.PRIVATE_KEY_LOCAL
 const API_KEY = keys.INFURA_API_KEY
 const NETWORK = "goerli"
 
 const hre = require("hardhat");
+const ethers = hre.ethers;
 
 async function main() {
+    await hre.run('compile'); // We are compiling the contracts using subtask
+    const [signer] = await ethers.getSigners(); // We are getting the deployer
+  
+    console.log('Deploying contracts with the account:', signer.address); // We are printing the address of the deployer
+    console.log('Account balance:', (await signer.getBalance()).toString()); // We are printing the account balance
 
-    const ETHWrapperFactory = await hre.ethers.getContractFactory("ETHWrapper");
-    const provider = new hre.ethers.providers.InfuraProvider(NETWORK, API_KEY);
-    const wallet = new hre.ethers.Wallet(PRIVATE_KEY, provider);
-    const balance = await wallet.getBalance();
+    // const wallet = new hre.ethers.Wallet(PRIVATE_KEY, signer);
+    const balance = await signer.getBalance();
 
     console.log(balance.toString())
-    const wrapValue = hre.ethers.utils.parseEther("1")
+    // const wrapValue = hre.ethers.utils.parseEther("1")
 
-    const ETHWrapperContract = await ETHWrapperFactory.attach("<Your ETHWrapper address>");
+    // ETHWrapperFactory
 
-    console.log(ETHWrapperContract.address)
+    // const ETHWrapperContract = await ETHWrapperFactory.attach("<Your ETHWrapper address>");
+
+    // console.log(ETHWrapperContract.address)
 
 }
 
